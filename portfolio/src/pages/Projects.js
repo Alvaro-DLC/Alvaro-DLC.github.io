8625f8979ef1a02ce0b295c047b84ed9527ac4fd
Alvaro De La Cruz <129230298+Alvaro-DLC@users.noreply.github.com>
2025-06-30 20:23:54 -0700

    initial steps to a good looking website

---
+++ portfolio/src/pages/Projects.js
@@ -0,0 +1,15 @@
+const Projects = () => {
+    return (
+        <div>
+            <div className="container">
+                <h1>Projects</h1>
+            </div>
+
+            <div className="container">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Projects;
